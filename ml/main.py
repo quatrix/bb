4bd567e7e6eecd576f589ef0ddbf7801cc7a7b9e
quatrix <evil.legacy@gmail.com>
2017-06-25 19:44:58 +0300

    Add base main.py

---
+++ ml/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import tensorflow as tf  # Version 1.0.0 (some previous versions are used in past commits)
+from sklearn import metrics
+
+import os
